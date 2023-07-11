e484c75c4769025a5b00d4c69409ff3a1164bce1
Asparuh Bambov <akbamboff@gmail.com>
2023-07-11 10:57:18 +0300

    changes to indentations.

--- JS Course/2. Basics/basics-02-added-scripts-imports/assets/scripts/app.js
+++ JS Course/2. Basics/basics-02-added-scripts-imports/assets/scripts/app.js
@@ -2,8 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function addsNumbers(num1, num2) {
-	const result = num1 + num2;
-	return result;
+  const result = num1 + num2;
+  return result;
 }
 
 currentResult = addsNumbers(1, 2);
