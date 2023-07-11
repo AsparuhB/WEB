ec2d301e0943523df677619ac7ec80ca5aaa9162
Asparuh Bambov <akbamboff@gmail.com>
2023-07-11 13:35:06 +0300

    Assignment completed.

--- JS Course/2. Basics/basics-assignment-2-start/assignment.js
+++ JS Course/2. Basics/basics-assignment-2-start/assignment.js
@@ -1,1 +1,23 @@
 const task3Element = document.getElementById('task-3');
+
+function simple() {
+    alert("Hello there!")
+    return;
+}
+
+function inputNeeded(name) {
+    alert(name);
+}
+
+inputNeeded("Alex")
+
+task3Element.addEventListener("click", simple);
+
+function threeStrings(str1, str2, str3) {
+    let stringimusPrime = str1 + str2 + str3;
+    return stringimusPrime;
+}
+
+threeStrings("az", " sum", " tuka");
+alert(threeStrings("az", " sum", " tuka"));
+
