1bd1e6f02cdc1081c1fc580196e3d99799b7fe89
Asparuh Bambov <akbamboff@gmail.com>
2023-07-12 11:07:59 +0300

    adding functions to all buttons

--- JS Course/2. Basics/basics-08-user-input-function/assets/scripts/app.js
+++ JS Course/2. Basics/basics-08-user-input-function/assets/scripts/app.js
@@ -13,3 +13,30 @@ function add() {
 }
 
 addBtn.addEventListener('click', add);
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const calcDesciption = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult,calcDesciption );
+}
+
+subtractBtn.addEventListener("click", subtract);
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const calcDesciption = `${currentResult} * ${enteredNumber}`;
+  currentResult = currentResult * enteredNumber;
+  outputResult(currentResult,calcDesciption );
+}
+
+multiplyBtn.addEventListener("click", multiply);
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const calcDesciption = `${currentResult} / ${enteredNumber}`;
+  currentResult = currentResult / enteredNumber;
+  outputResult(currentResult, calcDesciption);
+}
+
+divideBtn.addEventListener("click", divide);
