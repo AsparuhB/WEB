d6c0158e7a535355464826c78f01a5e1944f306a
Asparuh Bambov <akbamboff@gmail.com>
2023-07-13 10:43:06 +0300

    Making a function, that writes to the log.

--- JS Course/2. Basics/basics-08-user-input-function/assets/scripts/app.js
+++ JS Course/2. Basics/basics-08-user-input-function/assets/scripts/app.js
@@ -1,41 +1,51 @@
 const defaultResult = 0;
-let currentResult = defaultResult;
-let logEntries = [];
+let currentResult = defaultResult; // Current result variable.
+let logEntries = [];  // Array for log.
 
 // Gets input from input field
 function getUserNumberInput() {
-  return parseFloat(userInput.value);
-}
+  return parseFloat(userInput.value); 
+}   
 
-// Generates and writes calculation log.
+// Generates and writes a calculation log.
 function createAndWriteOutput(resultBefore, operator, calcNumber) {
   const calcDescription = `${resultBefore} ${operator} ${calcNumber}`;
-  outputResult(currentResult,calcDescription); // from vendor file!
-} 
+  outputResult(currentResult, calcDescription); // from vendor file!
+}
 
-// adds and inputs result.
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput(initialResult, "+", enteredNumber);
-  const logEntry = {
-    operation: "ADD",
-    prevResult: initialResult,
-    number: enteredNumber,
-    Result: currentResult
+// Writes to log.
+function writeToLog(  // Function with 4 parameters.
+  operationIdentifier,  // 1st parameter.
+  prevResult, // 2nd parameter.
+  operationNumber,  // 3rd parameter.
+  newResult // 4th parameter.
+) {
+  const logEntry = {  
+    operation: operationIdentifier, 
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
   };
-  logEntries.push(logEntry);
-  console.log(logEntry.operation);
-  console.log(logEntries);
+  logEntries.push(logEntry); // Pushing to array.
+  console.log(logEntries); // Logging array.
 }
 
 // No need for comment here, but test.
+function add() {  
+  const enteredNumber = getUserNumberInput(); 
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput(initialResult, "+", enteredNumber);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult)
+}
+
+// Same as above.
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput(initialResult, "-", enteredNumber);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult)
 }
 
 // Kind of self explanatory.
@@ -44,18 +54,20 @@ function multiply() {
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput(initialResult, "*", enteredNumber);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult)
 }
 
-// I mean... yeah...
+// Same as above.
 function divide() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult /= enteredNumber;
   createAndWriteOutput(initialResult, "/", enteredNumber);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult)
 }
 
 //Grouping all buttons and linking them to functions.
-addBtn.addEventListener('click', add);
+addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
 divideBtn.addEventListener("click", divide);
 multiplyBtn.addEventListener("click", multiply);
