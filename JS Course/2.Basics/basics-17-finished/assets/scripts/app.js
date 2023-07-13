6d2c7ba5332908341c22cbdff0691ea65bb0fa5c
Asparuh Bambov <akbamboff@gmail.com>
2023-07-13 12:37:55 +0300

    formatting my code a bit.

--- JS Course/2.Basics/basics-17-finished/assets/scripts/app.js
+++ JS Course/2.Basics/basics-17-finished/assets/scripts/app.js
@@ -1,67 +1,72 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-let logEntries = [];    // Array
+let logEntries = []; // Array
 
 // Gets input from input field
 function getUserNumberInput() {
   return parseInt(usrInput.value);
-}   // parseInt() converts string to number
+} // parseInt() converts string to number
 
 // Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); // from vendor file
-}   // `` is a template literal
+} // `` is a template literal
 
-function writeToLog(  // function to write to log
-  operationIdentifier, // string 
-  prevResult, // result before calculation  
+function writeToLog( // function to write to log
+  operationIdentifier, // string
+  prevResult, // result before calculation
   operationNumber, // number entered by user
   newResult // result after calculation
 ) {
-  const logEntry = {  // object
+  const logEntry = {
+    // object
     operation: operationIdentifier, // string
-    prevResult: prevResult,  // result before calculation
+    prevResult: prevResult, // result before calculation
     number: operationNumber, // number entered by user
-    result: newResult // result after calculation
+    result: newResult, // result after calculation
   }; // object
   logEntries.push(logEntry); // push() adds new element to end of array
   console.log(logEntries); // console.log() prints to console
 } // push() adds new element to end of array
 
-function add() { // function to add
+function add() {
+  // function to add
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
-  createAndWriteOutput('+', initialResult, enteredNumber);
-  writeToLog('ADD', initialResult, enteredNumber, currentResult);
-} 
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult);
+}
 
-function subtract() { // function to subtract
+function subtract() {
+  // function to subtract
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult -= enteredNumber;
-  createAndWriteOutput('-', initialResult, enteredNumber);
-  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
 }
 
-function multiply() { // function to multiply
-  const enteredNumber = getUserNumberInput(); 
+function multiply() {
+  // function to multiply
+  const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult *= enteredNumber;
-  createAndWriteOutput('*', initialResult, enteredNumber);
-  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
-} 
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+}
 
-function divide() { // function to divide
+function divide() {
+  // function to divide
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult /= enteredNumber;
-  createAndWriteOutput('/', initialResult, enteredNumber);
-  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
 }
 
-addBtn.addEventListener('click', add); // event listener
-subtractBtn.addEventListener('click', subtract); // event listener 
-multiplyBtn.addEventListener('click', multiply); // event listener
-divideBtn.addEventListener('click', divide);  // event listener
+addBtn.addEventListener("click", add); // event listener
+subtractBtn.addEventListener("click", subtract); // event listener
+multiplyBtn.addEventListener("click", multiply); // event listener
+divideBtn.addEventListener("click", divide); // event listener
