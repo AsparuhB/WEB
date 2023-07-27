bb0cac7120655d55a05afb264950dc5bf3f9d52e
Asparuh Bambov <akbamboff@gmail.com>
2023-07-27 12:50:01 +0300

    Subtle changes.

--- JS Course/5. Behind the Scenes and weird parts /2. Exercises./behind-scenes-01-starting-setup/app.js
+++ JS Course/5. Behind the Scenes and weird parts /2. Exercises./behind-scenes-01-starting-setup/app.js
@@ -1,7 +1,22 @@
-let name = "Asparuh";
+// let firstName = "Asparuh"; // GLOBAL variable - available everywhere
 
-function greet() {
-    console.log(name);
-}
+// if (firstName === "Asparuh") {
+//     let hobbies = ["Gaming", "Farting"];
+//     console.log(hobbies);
+// }
 
-greet();
+// function greet() {
+//     let age = 30; // LOCAL variable - available only in the function (or inside the curly braces)
+//     let name = "Manuel" // Thanks to shadowing we can declare the same name of a variable
+//     // inside a function and the global variable will be ignored. Instead the block scoped
+//     // name of the variable will be used.
+//     console.log(name) 
+// }
+ 
+// console.log(firstName);
+// greet();
+
+console.log(userName);
+
+let userName = "Max";
+ 
