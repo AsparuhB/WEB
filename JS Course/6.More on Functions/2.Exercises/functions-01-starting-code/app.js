b04020e66dbe3c7851743195f8b9de6def16c7a2
Asparuh Bambov <akbamboff@gmail.com>
2023-07-31 11:24:22 +0300

    clean sleight for the game

--- JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
+++ JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
@@ -1,19 +1,6 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function() {
+
+startGameBtn.addEventListener('click', function() {
     console.log("Game is starting...");
-}; // this is a function expression, not a declaration/statement. 
-// functions without names, stored in variables are also called anonymous expressions.
-
-// const person = {
-//     name: "Asparuh", // This is a property of the function.
-//     greet: function greet() {
-//         console.log("Hello there!");
-//     } // Functions, stored inside objects are called Methods. They are accessed by a .
-// };
-
-// person.greet();
-
-// console.dir(startGame);  
-
-startGameBtn.addEventListener('click', start)
+});
