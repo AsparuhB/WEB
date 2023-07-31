c45cb617d4e3bd692038336e923a5ae3b03932e1
Asparuh Bambov <akbamboff@gmail.com>
2023-07-31 10:29:51 +0300

    basic function and checking what is a method

--- JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
+++ JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
@@ -1,1 +1,18 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log("Game is starting...");
+}
+
+// const person = {
+//     name: "Asparuh", // This is a property of the function.
+//     greet: function greet() {
+//         console.log("Hello there!");
+//     } // Functions, stored inside objects are called Methods. They are accesed by a .
+// };
+
+// person.greet();
+
+console.dir(startGame);
+
+startGameBtn.addEventListener('click', startGame)
