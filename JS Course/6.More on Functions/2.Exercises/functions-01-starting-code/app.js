bf0bb1373c6460cd8d0a90efe81326e6616768b6
Asparuh Bambov <akbamboff@gmail.com>
2023-07-31 12:24:10 +0300

    Adding player choice. Catching errors.

--- JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
+++ JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
@@ -1,6 +1,46 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
 
 
-startGameBtn.addEventListener('click', function() {
-    console.log("Game is starting...");
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, "").toUpperCase();
+
+  try {
+    selection;
+  } catch (error) {
+    selection = DEFAULT_USER_CHOICE;
+  }
+  if (
+    selection !== ROCK && 
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+
+
+try {
+    getPlayerChoice();
+} catch (error) {
+    selection = DEFAULT_USER_CHOICE;
+}
+
+startGameBtn.addEventListener("click", function () {
+    if(gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+  console.log("Game is starting...");
+  const getPlayerSelection = getPlayerChoice();
+  console.log(getPlayerSelection);
 });
