687cc145cc6df7dcf89b1725feba06a4cd7dfb25
Asparuh Bambov <akbamboff@gmail.com>
2023-08-03 10:45:07 +0300

    first and second assignment done?

--- JS Course/6.More on Functions/3.Assignments/functions-assignment-problem/assignment.js
+++ JS Course/6.More on Functions/3.Assignments/functions-assignment-problem/assignment.js
@@ -2,4 +2,20 @@ const sayHello = (name) => {
   console.log('Hi ' + name);
 }
 
-sayHello();
+sayHello("Alex");
+
+const sayHello1 = (greet, name) => {
+  console.log(greet + name);
+}
+
+sayHello1("Hello, ", "Alex");
+
+const sayHello2 = () => {
+  console.log("What am I" + " doing ?");
+}
+
+sayHello2();
+
+const sayHello3 = (greet, name) => greet + name;
+
+console.log(sayHello3("Hello", " there!"));
