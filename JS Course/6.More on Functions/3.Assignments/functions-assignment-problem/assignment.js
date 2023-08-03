59b97310a42987ea160f17140188558642ce968d
Asparuh Bambov <akbamboff@gmail.com>
2023-08-03 15:12:18 +0300

    4th assignment.

--- JS Course/6.More on Functions/3.Assignments/functions-assignment-problem/assignment.js
+++ JS Course/6.More on Functions/3.Assignments/functions-assignment-problem/assignment.js
@@ -1,22 +1,48 @@
-// const sayHello = (name) => {
-//   console.log('Hi ' + name);
+const sayHello = (name) => console.log("Hi, " + name);
+const sayHello2 = (greet, name) => console.log(greet + " " + name);
+const sayHello3 = () => console.log("What am I doing ?");
+const sayHello4 = (name) => "Hi, " + name;
+
+sayHello("Alex");
+sayHello2("Hello,", "there!");
+sayHello3();
+console.log(sayHello4("Alex"));
+
+const sayHello5 = (greet = "Hi,", name = "Alex") =>
+  console.log(greet + " " + name);
+
+sayHello5();
+sayHello5("Hello,", "Asparuh");
+
+// let x = () => {
+//   console.log("I am called from inside of a function.");
+// };
+
+// let y = (callBack) => {
+//   console.log("Do something");
+//   callBack();
 // }
 
-// sayHello("Alex");
+// y(x)
 
-// const sayHello2 = () => {
-//   console.log("What am I" + " doing ?");
-// }
-
-// sayHello2();
-
-// const sayHello3 = (greet, name) => greet + name;
-
-// console.log(sayHello3("Hello", " there!"));
-
-const sayHello1 = (greet, name = "Alex") => {
-  console.log(greet + name);
+function checkInput(callBack, ...strings) {
+  let hasEmptyText = false;
+  for (const text of strings) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
+    }
+  }
+  if (!hasEmptyText) {
+    return callBack();
+  }
 }
 
-sayHello1("Hello, ");
-sayHello1("Hello, ", "George");
+checkInput(
+  () => {
+    console.log("Not all empty");
+  },
+  "t",
+  "rt",
+  "g"
+);
