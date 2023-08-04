7ab0d73d8907e3c0fa9a987a9e137e9f9134fdb2
Asparuh Bambov <akbamboff@gmail.com>
2023-08-04 14:29:19 +0300

    Shortening the functions

--- JS Course/2.Basics/basics-08-user-input-function/assets/scripts/app.js
+++ JS Course/2.Basics/basics-08-user-input-function/assets/scripts/app.js
@@ -1,11 +1,16 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // Current result variable.
-let logEntries = [];  // Array for log.
+let logEntries = []; // Array for log.
+
+const ADD = "ADD";
+const SUBTRACT = "SUBTRACT";
+const MULTIPLY = "MULTIPLY";
+const DIVIDE = "DIVIDE";
 
 // Gets input from input field
 function getUserNumberInput() {
-  return parseFloat(userInput.value); 
-}   
+  return parseFloat(userInput.value);
+}
 
 // Generates and writes a calculation log.
 function createAndWriteOutput(resultBefore, operator, calcNumber) {
@@ -14,14 +19,14 @@ function createAndWriteOutput(resultBefore, operator, calcNumber) {
 }
 
 // Writes to log.
-function writeToLog(  // Function with 4 parameters.
-  operationIdentifier,  // 1st parameter.
+function writeToLog( // Function with 4 parameters.
+  operationIdentifier, // 1st parameter.
   prevResult, // 2nd parameter.
-  operationNumber,  // 3rd parameter.
+  operationNumber, // 3rd parameter.
   newResult // 4th parameter.
 ) {
-  const logEntry = {  
-    operation: operationIdentifier, 
+  const logEntry = {
+    operation: operationIdentifier,
     prevResult: prevResult,
     number: operationNumber,
     result: newResult,
@@ -31,43 +36,64 @@ function writeToLog(  // Function with 4 parameters.
 }
 
 // No need for comment here, but test.
-function add() {  
-  const enteredNumber = getUserNumberInput(); 
-  const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput(initialResult, "+", enteredNumber);
-  writeToLog("ADD", initialResult, enteredNumber, currentResult)
-}
+// function add() {
+//   const enteredNumber = getUserNumberInput();
+//   const initialResult = currentResult;
+//   currentResult += enteredNumber;
+//   createAndWriteOutput(initialResult, "+", enteredNumber);
+//   writeToLog("ADD", initialResult, enteredNumber, currentResult)
+// }
 
-// Same as above.
-function subtract() {
+// // Same as above.
+// function subtract() {
+//   const enteredNumber = getUserNumberInput();
+//   const initialResult = currentResult;
+//   currentResult -= enteredNumber;
+//   createAndWriteOutput(initialResult, "-", enteredNumber);
+//   writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult)
+// }
+
+// // Kind of self explanatory.
+// function multiply() {
+//   const enteredNumber = getUserNumberInput();
+//   const initialResult = currentResult;
+//   currentResult *= enteredNumber;
+//   createAndWriteOutput(initialResult, "*", enteredNumber);
+//   writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult)
+// }
+
+// // Same as above.
+// function divide() {
+//   const enteredNumber = getUserNumberInput();
+//   const initialResult = currentResult;
+//   currentResult /= enteredNumber;
+//   createAndWriteOutput(initialResult, "/", enteredNumber);
+//   writeToLog("DIVIDE", initialResult, enteredNumber, currentResult)
+// }
+
+function calculate(operation) {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteOutput(initialResult, "-", enteredNumber);
-  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult)
-}
-
-// Kind of self explanatory.
-function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput(initialResult, "*", enteredNumber);
-  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult)
-}
-
-// Same as above.
-function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput(initialResult, "/", enteredNumber);
-  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult)
+  let operator;
+  if (operation === ADD) {
+    currentResult += enteredNumber;
+    operator = "+";
+  } else if (operation === SUBTRACT) {
+    currentResult -= enteredNumber;
+    operator = "-";
+  } else if (operation === MULTIPLY) {
+    currentResult *= enteredNumber;
+    operator = "*";
+  } else if (operation === DIVIDE) {
+    currentResult /= enteredNumber;
+    operator = "/";
+  }
+  createAndWriteOutput(initialResult, operator, enteredNumber);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
 //Grouping all buttons and linking them to functions.
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-divideBtn.addEventListener("click", divide);
-multiplyBtn.addEventListener("click", multiply);
+addBtn.addEventListener("click", calculate.bind(this, ADD));
+subtractBtn.addEventListener("click", calculate.bind(this, SUBTRACT));
+divideBtn.addEventListener("click", calculate.bind(this, DIVIDE));
+multiplyBtn.addEventListener("click", calculate.bind(this, MULTIPLY));
