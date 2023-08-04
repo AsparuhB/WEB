0178e062d68cadffba2f8bfe7133e33c8a039361
Asparuh Bambov <akbamboff@gmail.com>
2023-08-04 11:22:33 +0300

    introducing .bind() method

--- JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
+++ JS Course/6.More on Functions/2.Exercises/functions-01-starting-code/app.js
@@ -74,31 +74,61 @@ startGameBtn.addEventListener("click", () => {
 
 // Not related to the game.
 
-const sumUp = (resultHandler, ...numbers) => {
-    const validateNumber = (number) => {
-        return isNaN(number) ? 0 : number;
-    };
-    
+const combine = (resultHandler, operation, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function() {
-    let sum = 0;
-    for (const num of arguments) { // Don't use that.
-      sum -= num;
-    }
-    return sum;
-  };
+// const subtractUp = function(resultHandler, ...numbers) {
+//     let sum = 0;
+//     for (const num of numbers) { // Don't use that.
+//       sum -= num;
+//     }
+//     resultHandler(sum);
+//   };
 
-  const showResult = (result) => {
-    alert("The result after adding all numbers is: " + result);
-  };
+const showResult = (result, messageText) => {
+  alert(messageText + " " + result);
+};
 
-
-sumUp(showResult, 1, 5, "sds", -3, 6, 10);
-sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
-console.log(subtractUp(1, 10, 15,20));
+combine(
+  showResult.bind(this, "The result after adding all numbers is:"),
+  "ADD",
+  1,
+  5,
+  "sds",
+  -3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, "The result after adding all numbers is:"),
+  "ADD",
+  1,
+  5,
+  10,
+  -3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, "The result after subracting all numbers is:"),
+  "SUBTRACT",
+  1,
+  10,
+  15,
+  20
+);
