addd42c1328ee33cd1feb565676c2773e8448cb7
Asparuh Bambov <akbamboff@gmail.com>
2023-08-08 12:25:53 +0300

    Selecting Ele from JS file.

--- JS Course/7. Working with the DOM model in JS/2. Exercises./dom-01-starting-code/app.js
+++ JS Course/7. Working with the DOM model in JS/2. Exercises./dom-01-starting-code/app.js
@@ -1,3 +1,15 @@
+const h1 = document.getElementById("main-title");
+
+h1.textContent = "Some new title!";
+h1.style.color = "white";
+h1.style.backgroundColor = "black";
+
+const lastLi = document.querySelector("li:last-of-type");
+lastLi.textContent = lastLi.textContent + " (Changed!)";
+
+const body = document.body;
+
+
 const listItemElement = document.querySelectorAll("li");
 
 for (const listItemEl of listItemElement) {
