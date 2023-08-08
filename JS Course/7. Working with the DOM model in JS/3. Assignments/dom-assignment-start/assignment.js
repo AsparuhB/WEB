6f942389707c1e98d4431722ed5a2f20ae3bee12
Asparuh Bambov <akbamboff@gmail.com>
2023-08-08 13:02:17 +0300

    completed DOM assignemnt

---
+++ JS Course/7. Working with the DOM model in JS/3. Assignments/dom-assignment-start/assignment.js
@@ -0,0 +1,27 @@
+// Task 1
+const task1 = document.querySelector("li");
+task1.style.color = "white";
+
+const task1opt2 = document.getElementById('task-1');
+task1opt2.style.backgroundColor = "black";
+
+// Task 2
+// // Way 1
+// const headTitle = document.head;
+// const title1 = headTitle.querySelector("title");
+// title1.textContent = "Assignment - Solved!";
+
+// Way 2 
+const titleInHead = document.querySelector("title")
+titleInHead.textContent = "Assignment - Solved";
+
+// Task 3
+// Way 1
+const h1opt1 = document.querySelector("h1");
+h1opt1.textContent = "Assignment - Solved!";
+
+// // Way 2
+// const body = document.body;
+// const titleHead = body.querySelector("h1");
+
+// titleHead.textContent = "Assignment - Solved";
