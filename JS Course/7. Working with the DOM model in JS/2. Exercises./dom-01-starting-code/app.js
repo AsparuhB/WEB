0e41f8dc855705d099a9740db9b7bd46905911ae
Asparuh Bambov <akbamboff@gmail.com>
2023-08-10 13:28:51 +0300

    various changes and interesting things added

--- JS Course/7. Working with the DOM model in JS/2. Exercises./dom-01-starting-code/app.js
+++ JS Course/7. Working with the DOM model in JS/2. Exercises./dom-01-starting-code/app.js
@@ -1,6 +1,8 @@
 const section = document.querySelector("section");
 const button = document.querySelector("button");
 
+const div = document.querySelector("div:last-of-type")
+
 // section.style.backgroundColor = "blue";
 
 section.className = "red-bg";
@@ -14,4 +16,26 @@ button.addEventListener("click", () => {
 
   // section.classList.toggle("visible");
   section.classList.toggle("invisible");
+});
+
+const slapIt = document.querySelector("#slap");
+const input = document.querySelector("input");
+
+slapIt.addEventListener("click", () => {
+    const textValue = input.value;
+    div.innerHTML =`<p class="newText">${textValue}</p>`;
+})
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  document.location.reload();
+});
+
+
+const inputId = document.getElementById('input-id');
+inputId.addEventListener('keydown', function onEvent(e) {
+    if (e.keyCode === 13) {
+        const textValue = input.value;
+    div.innerHTML =`<p class="newText">${textValue}</p>`;
+    }
 });
