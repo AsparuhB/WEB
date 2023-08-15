e25ca5bc9d42383d1c2d7daea1d49d521eb22823
Asparuh Bambov <akbamboff@gmail.com>
2023-08-15 13:35:51 +0300

    adding some functionalities.

--- JS Course/7. Working with the DOM model in JS/2. Exercises./MOVIE PROJECT/assets/scripts/app.js
+++ JS Course/7. Working with the DOM model in JS/2. Exercises./MOVIE PROJECT/assets/scripts/app.js
@@ -1,8 +1,25 @@
-const startAddMovieButton = document.querySelector('header button');
-const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector("header button");
+const addMovieModal = document.getElementById("add-modal");
+const backdrop = document.getElementById("backdrop");
+const cancelButton =
+  document.querySelector(".modal__actions").firstElementChild;
 
-const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible');
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle("visible");
+    toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelButton.addEventListener("click", toggleMovieModal);
+
