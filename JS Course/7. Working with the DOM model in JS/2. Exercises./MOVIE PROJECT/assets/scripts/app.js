e3063c5d3571c9e77944c136cd7a0749f79863cd
Asparuh Bambov <akbamboff@gmail.com>
2023-08-16 10:26:02 +0300

    adding an updating UI functionality

--- JS Course/7. Working with the DOM model in JS/2. Exercises./MOVIE PROJECT/assets/scripts/app.js
+++ JS Course/7. Working with the DOM model in JS/2. Exercises./MOVIE PROJECT/assets/scripts/app.js
@@ -4,8 +4,34 @@ const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
 
 const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block"
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div>
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    </div>
+  `
+  const listRoot = document.getElementById("movie-list");
+  listRoot.append(newMovieElement);
+}
+
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -14,6 +40,7 @@ const toggleBackdrop = () => {
 const cancelAddMovieHandler = () => {
   toggleMovieModal();
   clearMovieInputs();
+  updateUI();
 };
 
 const clearMovieInputs = () => {
@@ -53,6 +80,9 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInputs();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
+
 };
 
 const backdropClickHandler = () => {
