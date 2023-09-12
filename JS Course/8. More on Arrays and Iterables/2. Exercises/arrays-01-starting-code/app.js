c51a52b39bb9afe278ace70ec7fb03f2e0e9ef85
Asparuh Bambov <akbamboff@gmail.com>
2023-09-12 10:02:53 +0300

    push

--- JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
+++ JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
@@ -1,25 +1,34 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = Array(5);
-// console.log(moreNumbers);
+// // const moreNumbers = Array(5);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of();
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of();
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li')
-console.log(listItems);
+// const listItems = document.querySelectorAll('li')
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems)
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems)
 
-const moreNumbers = Array.from('Hi!');
-console.log(moreNumbers); 
+// const moreNumbers = Array.from('Hi!');
+// console.log(moreNumbers); 
 
-const analyticsData = [[1, 1,6], [-5.4, 2.1]];
+// const analyticsData = [[1, 1,6], [-5.4, 2.1]];
 
-for (const data of analyticsData) {
-    for (const dataPoint of data) {
-        console.log(dataPoint);
-    }
-}
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
+
+const hobbies = ["Sports", "Cooking"];
+
+hobbies.push("Reading");
+hobbies.unshift("Coding");
+console.log(hobbies);
+
+let popped = hobbies.pop();
+console.log(popped)
