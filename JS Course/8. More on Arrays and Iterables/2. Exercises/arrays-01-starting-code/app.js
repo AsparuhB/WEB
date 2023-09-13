5eeb9191bb6f0db9baa7e626d8be1b3d2962cb48
Asparuh Bambov <akbamboff@gmail.com>
2023-09-13 11:31:13 +0300

    cs

--- JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
+++ JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
@@ -66,16 +66,32 @@
 
 // console.log(maxIndex)
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-const taxAdjustedPrices = [];
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));
-// }
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // }
 
-prices.forEach((price, idx, prices) => {
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
+// console.log(taxAdjustedPrices);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+
+const taxAdjustedPrices =  prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
+
+const something = ["Something 1", "Something 2", "Something 3", "Something 4" ];
+
+const removedSomething = something.splice(1, 2);
+
+console.log(removedSomething);
