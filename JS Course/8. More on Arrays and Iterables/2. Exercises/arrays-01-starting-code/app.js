ed8552281adbb1d3ce69a182a08edb13e603563f
Asparuh Bambov <akbamboff@gmail.com>
2023-09-13 13:03:07 +0300

    various methods added.

--- JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
+++ JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/app.js
@@ -80,23 +80,44 @@
 // });
 // console.log(taxAdjustedPrices);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
+// const taxAdjustedPrices =  prices.map((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   return priceObj;
+// });
+// // console.log(prices, taxAdjustedPrices);
 
-const taxAdjustedPrices =  prices.map((price, idx, prices) => {
-  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-  return priceObj;
-});
-// console.log(prices, taxAdjustedPrices);
+// const sortedPrices = prices.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     } else if (a === b) {
+//         return 0;
+//     } else {
+//         return -1
+//     }
+// });
+// console.log(sortedPrices);
 
-const sortedPrices = prices.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a === b) {
-        return 0;
-    } else {
-        return -1
-    }
-});
-console.log(sortedPrices);
+// const filteredArray = prices.filter(price => price > 6);
+
+// console.log(filteredArray)
+
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //     sum += price;
+// // })
+// // console.log(sum);
+
+// const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+// console.log(sum)
+
+const originalArray = [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }];
+
+const sum = originalArray.map(obj => obj.price).reduce((sumVal, curVal) => sumVal + curVal, 0);
+
+console.log(originalArray);
+console.log(sum);
