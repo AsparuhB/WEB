ca18439085178f2302dcf225db2c3e15bf06dc57
Asparuh Bambov <akbamboff@gmail.com>
2023-09-18 11:36:46 +0300

    various

---
+++ JS Course/8. More on Arrays and Iterables/2. Exercises/arrays-01-starting-code/setsmaps.js
@@ -0,0 +1,23 @@
+// const ids = new Set(["Hi", "from", "set!"]);
+// ids.add(2);
+
+// if (ids.has("Hi")) {
+//   ids.delete("Hi");
+// }
+
+// console.log(ids.has(1));
+// for (const entry of ids.entries()) {
+//   console.log(entry);
+// }
+
+// console.log(ids.values());
+
+// const setArray = Array.from(ids);
+// console.log(setArray);
+
+const person1 = {name: "Max"};
+const person2 = {name: "Manuel"};
+
+const personData = new Map([[person1, [{date: "yesterday", price: 10}]]]); 
+
+console.log(personData);
