9e0110a67213f7c7fc6b8c97598827fc3d158314
Asparuh Bambov <akbamboff@gmail.com>
2023-10-13 12:43:58 +0300

    CMOOON

--- JS Course/10.Classes & Object-oriented Programming (OOP)/2. Exercises/oop-01-starting-setup/assets/scripts/app.js
+++ JS Course/10.Classes & Object-oriented Programming (OOP)/2. Exercises/oop-01-starting-setup/assets/scripts/app.js
@@ -68,7 +68,6 @@ class ProductList {
   constructor() {}
 
   render() {
-    const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
@@ -76,9 +75,24 @@ class ProductList {
       const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList
   }
 }
 
-const productList = new ProductList();
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl)
+    renderHook.append(prodListEl)
+  }
+}
+
+const shop = new Shop();
+shop.render();
