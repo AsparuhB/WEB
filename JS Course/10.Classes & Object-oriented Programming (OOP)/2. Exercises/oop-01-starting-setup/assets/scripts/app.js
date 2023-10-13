231edc4ba154afd7b58efe37779d50aac0d638fb
Asparuh Bambov <akbamboff@gmail.com>
2023-10-13 11:33:59 +0300

    dig a hole, dig a hole

--- JS Course/10.Classes & Object-oriented Programming (OOP)/2. Exercises/oop-01-starting-setup/assets/scripts/app.js
+++ JS Course/10.Classes & Object-oriented Programming (OOP)/2. Exercises/oop-01-starting-setup/assets/scripts/app.js
@@ -20,13 +20,12 @@ const productList = {
       "A soft pillow!",
       19.99
     ),
-    {
-      title: "A carpet",
-      imageUrl:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2F1bcc7c86-c2e1-4bb6-bbbb-260c4b52215e.a41b8d66257d48ca92eb211e6ebf0df2.jpeg&f=1&nofb=1&ipt=03dd9ef2bd740ba765f197df88c41b31a332e945bb0de3d348b72a837fd05194&ipo=images",
-      price: 89.99,
-      description: "A carpet which you might like - or not.",
-    },
+    new Product(
+      "A carpet",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2F1bcc7c86-c2e1-4bb6-bbbb-260c4b52215e.a41b8d66257d48ca92eb211e6ebf0df2.jpeg&f=1&nofb=1&ipt=03dd9ef2bd740ba765f197df88c41b31a332e945bb0de3d348b72a837fd05194&ipo=imagestps://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dumas-paris.com%2F93-thickbox_default%2Fimperial-90-goose-down-pillow.jpg&f=1&nofb=1&ipt=34e6c2fbf86ca71690e579103d5a4e9c2686c814f08e60f16563f8adb854d0ab&ipo=images",
+      "A carpet which you might like - or not.",
+      89.99
+    ),
   ],
 
   render() {
