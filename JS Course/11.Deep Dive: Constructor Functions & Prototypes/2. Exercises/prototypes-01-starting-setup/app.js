eb961cce12d52d9df0c6bbbd6d5d61421a66a4d5
Asparuh Bambov <akbamboff@gmail.com>
2023-10-24 12:23:15 +0300

    fist thing

---
+++ JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+class Person {
+  name = "Alex";
+
+  constructor() {
+    this.age = 30;
+  }
+
+  greet() {
+    console.log(
+      "Hi, I am " + this.name + " and I am " + this.age + " years old."
+    );
+  }
+}
+
+const person = new Person();
+person.greet();
