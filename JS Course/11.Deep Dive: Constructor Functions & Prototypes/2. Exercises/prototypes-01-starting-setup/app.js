6d9762822873e56fa36db4c214e7d50497848b2d
Asparuh Bambov <akbamboff@gmail.com>
2023-10-24 12:35:52 +0300

    Introducing constructor functions

--- JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
+++ JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
@@ -1,15 +1,25 @@
-class Person {
-  name = "Alex";
+// class Person {
+//   name = "Alex";
 
-  constructor() {
+//   constructor() {
+//     this.age = 30;
+//   }
+
+//   greet() {
+//     console.log(
+//       "Hi, I am " + this.name + " and I am " + this.age + " years old."
+//     );
+//   }
+// }
+
+function Person() {
     this.age = 30;
-  }
-
-  greet() {
-    console.log(
-      "Hi, I am " + this.name + " and I am " + this.age + " years old."
-    );
-  }
+    this.name = "Alex";
+    this.greet = function() {
+        console.log(
+                  "Hi, I am " + this.name + " and I am " + this.age + " years old."
+                );
+    }
 }
 
 const person = new Person();
