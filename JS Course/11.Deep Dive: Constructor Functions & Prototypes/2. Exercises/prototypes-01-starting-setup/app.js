6069bc072b34ce0b1a457130ce4fec8f98564171
Asparuh Bambov <akbamboff@gmail.com>
2023-10-25 11:04:40 +0300

    psspss

--- JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
+++ JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
@@ -13,14 +13,24 @@
 // }
 
 function Person() {
-    this.age = 30;
-    this.name = "Alex";
-    this.greet = function() {
-        console.log(
-                  "Hi, I am " + this.name + " and I am " + this.age + " years old."
-                );
-    }
+  this.age = 30;
+  this.name = "Alex";
+  this.greet = function () {
+    console.log(
+      "Hi, I am " + this.name + " and I am " + this.age + " years old."
+    );
+  };
 }
+
+Person.prototype = {
+  printAge() {
+    console.log(this.age);
+  },
+};
+
+console.dir(Person);
 
 const person = new Person();
 person.greet();
+person.printAge();
+console.log(person.__proto__);
