252377ce83a4f58ea0de14d149d71ac7d3533dad
Asparuh Bambov <akbamboff@gmail.com>
2023-10-27 11:32:59 +0300

    strange behavior

--- JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
+++ JS Course/11.Deep Dive: Constructor Functions & Prototypes/2. Exercises/prototypes-01-starting-setup/app.js
@@ -1,33 +1,33 @@
-// class AgedPerson {
-//   printAge() {
-//     console.log(this.age);
-//   }
-// }
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
 
-// class Person extends AgedPerson {
-//   name = "Alex";
+class Person extends AgedPerson {
+  name = "Alex";
 
-//   constructor() {
-//     super()
-//     this.age = 30;
-//   }
+  constructor() {
+    super()
+    this.age = 30;
+  }
 
-//   greet() {
-//     console.log(
-//       "Hi, I am " + this.name + " and I am " + this.age + " years old."
-//     );
-//   }
-// }
-
-function Person() {
-  this.age = 30;
-  this.name = "Alex";
-  this.greet = function () {
+  greet() {
     console.log(
       "Hi, I am " + this.name + " and I am " + this.age + " years old."
     );
-  };
+  }
 }
+
+// function Person() {
+//   this.age = 30;
+//   this.name = "Alex";
+//   this.greet = function () {
+//     console.log(
+//       "Hi, I am " + this.name + " and I am " + this.age + " years old."
+//     );
+//   };
+// }
 
 // Person.prototype = {
 //   // setting a prototype
@@ -36,22 +36,25 @@ function Person() {
 //   },
 // };
 
-Person.prototype.printAge = function () {
-  console.log(this.age);
-};
+// Person.prototype.printAge = function () {
+//   console.log(this.age);
+// };
 
-Person.describe = function () {
-  console.log("Creating persons...");
-};
+// Person.describe = function () {
+//   console.log("Creating persons...");
+// };
 
-console.dir(Person);
+// console.dir(Person);
 
-const person = new Person();
-person.greet();
-person.printAge();
-console.log(person.__proto__);
-console.log(person.toString());
-const p2 = new person.__proto__.constructor();
-console.log(p2);
+// const person = new Person();
+// person.greet();
+// person.printAge();
+// console.log(person.__proto__);
+// console.log(person.toString());
+// const p2 = new person.__proto__.constructor();
+// console.log(p2);
 
-console.dir(Object.prototype);
+// console.dir(Object.prototype);
+
+const p = new Person();
+console.log(p)
