e9a893ef0531a4972f00403813da053533cb2c37
Asparuh Bambov <116065988+AsparuhB@users.noreply.github.com>
2023-11-08 12:38:21 +0200

    Section 14  working with events (#19)
    * Uploading first file for events
    
    * working with event handlers
    
    * adding another button
    
    * listening to other events
    
    * Forms and submit. preventDefault()

---
+++ JS Course/14.Working with Events/2. Exercises/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll("button");
+
+// button.onclick = function () {
+//   alert("Hello");
+// };
+
+const buttonClickHandler = (event) => {
+  // event.target.disabled = true;
+  console.log(event);
+};
+
+const anotherButtonClickHandler = () => {
+  console.log("This was clicked!");
+};
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+
+const boundFn = buttonClickHandler.bind(this);
+
+// button.addEventListener("click", buttonClickHandler);
+
+// setTimeout(() => {
+//   button.removeEventListener("click", buttonClickHandler);
+// }, 2000);
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", buttonClickHandler);
+//   btn.addEventListener('mouseleave', () => {
+//     btn.style.visibility = 'hidden'
+//   })
+// });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
