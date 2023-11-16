397f2f5428f74c18abb6130f6bdccce1a32f3ed5
Asparuh Bambov <akbamboff@gmail.com>
2023-11-16 11:12:51 +0200

    introducing factory functions

--- JS Course/15. Advanced Function Concepts/2. Exercises/adv-fn-01-starting-setup/app.js
+++ JS Course/15. Advanced Function Concepts/2. Exercises/adv-fn-01-starting-setup/app.js
@@ -1,13 +1,51 @@
-function add(num1, num2) {
-  return num1 + num2;
-} // This is a pure function. Yield the same result every time, assuming the same parameters are inputted 
+// function add(num1, num2) {
+//   return num1 + num2;
+// } // This is a pure function. Yield the same result every time, assuming the same parameters are inputted
 
-console.log(add(1, 5)) // 6
-console.log(add(12, 15)) // 27
+// console.log(add(1, 5)); // 6
+// console.log(add(12, 15)); // 27
 
-// This is an impure function. Yields unpredictable results even when the input is the same.
-function addRandom(num1) {
-  return num1 + Math.random();
+// // This is an impure function. Yields unpredictable results even when the input is the same.
+// function addRandom(num1) {
+//   return num1 + Math.random();
+// }
+
+// console.log(addRandom(5));
+
+// let previousResult = 0;
+// console.log(previousResult);
+
+// function addMoreNumbers(num1, num2) {
+//   const sum = num1 + num2;
+//   previousResult = sum; // This is a side effect, so this is also an impure function, because it changes a variable OUTSIDE of the function
+//   return sum;
+// }
+
+// console.log(addMoreNumbers(5, 6)); // if called like this, it might not be obvious
+// // that this function changes something outside of it. MIGHT WANT TO AVOID!!!
+
+// console.log(previousResult);
+
+// const hobbies = ["Sports", "Cooking"];
+
+// function printHobbies(h) {
+//   h.push("NEW HOBBY");
+//   console.log(h);
+// }
+
+// printHobbies(hobbies); // when we change an array inside of the function, we change it also outside of the function, because stored arrays are reference to the objects location.
+
+// Factory Functions:
+
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+  return calculateTax;
 }
 
-console.log(addRandom(5))
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100))
+console.log(calculateVatAmount(200))
