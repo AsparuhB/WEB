dc5c4d1c3fbd7fb4a510b572086f49ec6c49d91c
Asparuh Bambov <akbamboff@gmail.com>
2023-11-16 11:50:45 +0200

    scoping and shit.

--- JS Course/15. Advanced Function Concepts/2. Exercises/adv-fn-01-starting-setup/app.js
+++ JS Course/15. Advanced Function Concepts/2. Exercises/adv-fn-01-starting-setup/app.js
@@ -37,9 +37,12 @@
 
 // Factory Functions:
 
+let multiplier = 1.1;
+
 function createTaxCalculator(tax) {
   function calculateTax(amount) {
-    return amount * tax;
+    console.log(multiplier)
+    return amount * tax * multiplier;
   }
   return calculateTax;
 }
@@ -47,5 +50,8 @@ function createTaxCalculator(tax) {
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
 
-console.log(calculateVatAmount(100))
-console.log(calculateVatAmount(200))
+multiplier = 1.2
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
+
