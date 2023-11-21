8cdcf70fa6d537d24bddd0faffab0884ec5f0a0a
Asparuh Bambov <akbamboff@gmail.com>
2023-11-21 20:53:41 +0200

    Darn ;

--- JS Course/16. More on Numbers & Strings/2. Exercises/nums-strings-01-starting-setup/app.js
+++ JS Course/16. More on Numbers & Strings/2. Exercises/nums-strings-01-starting-setup/app.js
@@ -8,22 +8,22 @@ console.log(randomIntBetween(1, 10));
 // TAGGED TEMPLATE Holy God!
 
 function productDescription(strings, productName, productPrice) {
-  console.log(strings)
-  console.log(productName)
-  console.log(productPrice)
-  
-  let priceCategory = `cheap for the price`
-  
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+
+  let priceCategory = `cheap for the price`;
+
   if (productPrice > 20) {
-    priceCategory = 'fair price'
+    priceCategory = "fair price";
   }
-  
-  return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`
+
+  return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
 }
 
-const prodName = 'JavaScript Course'
-const prodPrice = 29.99
+const prodName = "JavaScript Course";
+const prodPrice = 29.99;
 
 const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
 
-console.log(productOutput)
+console.log(productOutput);
