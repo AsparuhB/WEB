5c15696f1ae280ae36035f82026f737bb5db6058
Asparuh Bambov <akbamboff@gmail.com>
2023-11-21 20:14:00 +0200

    working with Math. and template literal

---
+++ JS Course/16. More on Numbers & Strings/2. Exercises/nums-strings-01-starting-setup/app.js
@@ -0,0 +1,13 @@
+function randomIntBetween(min, max) {
+  // min: 5 , max: 10
+  return Math.floor(Math.random() * (max - min + 1) + min); // 0,999999 => 1 is not included.
+}
+
+console.log(randomIntBetween(1, 10));
+
+
+// TAGGED TEMPLATE
+
+function productDescription(strings, productName, productPrice) {
+  return "This is a product!"
+}
