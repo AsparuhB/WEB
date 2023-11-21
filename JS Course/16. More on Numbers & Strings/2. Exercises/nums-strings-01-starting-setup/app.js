6abb36b21038ba9417819139adbbc9a8282d619c
Asparuh Bambov <akbamboff@gmail.com>
2023-11-21 20:42:49 +0200

    Tagged Template...

--- JS Course/16. More on Numbers & Strings/2. Exercises/nums-strings-01-starting-setup/app.js
+++ JS Course/16. More on Numbers & Strings/2. Exercises/nums-strings-01-starting-setup/app.js
@@ -7,3 +7,16 @@ console.log(randomIntBetween(1, 10));
 
 // TAGGED TEMPLATE Holy God!
 
+function productDescription(strings, productName, productPrice) {
+  console.log(strings)
+  console.log(productName)
+  console.log(productPrice)
+  return "This is a product"
+}
+
+const prodName = 'JavaScript Course'
+const prodPrice = 29.99
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+
+console.log(productOutput)
