74372aff3d3247273c7cead4799fc5f381a87100
Asparuh Bambov <akbamboff@gmail.com>
2023-11-22 19:08:13 +0200

    showing some async functions

--- JS Course/17. Async JS: Promises and Callbacks/2. Exercises/async-01-starting-setup/app.js
+++ JS Course/17. Async JS: Promises and Callbacks/2. Exercises/async-01-starting-setup/app.js
@@ -2,15 +2,20 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimeout(() => {
+        console.log(posData);
+      }, 2000);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+  setTimeout(() => {
+    console.log("Timer Done!")
+  }, 0)
+  console.log('Getting position... Please wait.');
 }
 
 button.addEventListener('click', trackUserHandler); // Async task, handed to the browser and handled by it.
-
-let result = 0;
-
-for (let i = 0; i < 100000000; i++) {
-  result += i;
-}
-
-console.log(result);
