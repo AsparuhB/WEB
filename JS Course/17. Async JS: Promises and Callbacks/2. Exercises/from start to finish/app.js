8efd1d87ddec8b2fd433608280e4ce0bb6c1fb1b
Asparuh Bambov <akbamboff@gmail.com>
2023-11-22 22:48:16 +0200

    Adding all the exercises.

---
+++ JS Course/17. Async JS: Promises and Callbacks/2. Exercises/from start to finish/app.js
@@ -0,0 +1,38 @@
+const button = document.querySelector('button');
+const output = document.querySelector('p');
+
+// introducing promises
+const getPosition = (opts) => {
+  const posPromise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return posPromise;
+};
+
+const setTimer = async (duration) => {
+  // async on expressions
+  const timerPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!'); // fn executed by the browser, once the fn is complete.
+    }, duration);
+  });
+  return timerPromise;
+};
+
+// promise chaining
+async function trackUserHandler() {
+  // async on declarations
+  const posData = await getPosition();
+  const timerData = await setTimer(2000); // await keyword.
+  console.log(timerData, posData)
+}
+
+button.addEventListener('click', trackUserHandler); // Async task, handed to the browser and handled by it.
