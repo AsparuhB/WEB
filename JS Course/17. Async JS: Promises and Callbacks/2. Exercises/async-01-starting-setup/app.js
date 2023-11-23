1ca2ef84381664cb28c4407e68b3e4d1eaf11001
Asparuh Bambov <akbamboff@gmail.com>
2023-11-23 12:44:26 +0200

    promisifying some built in methods.

--- JS Course/17. Async JS: Promises and Callbacks/2. Exercises/async-01-starting-setup/app.js
+++ JS Course/17. Async JS: Promises and Callbacks/2. Exercises/async-01-starting-setup/app.js
@@ -1,20 +1,29 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const timerPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return timerPromise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout((data) => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (err) => {
       console.log(err);
     }
   );
-  setTimeout(() => {
-    console.log('Timer done...');
-  }, 0);
+  setTimer(1000).then((data) => {
+    console.log(data);
+  });
   console.log('Getting position...'); // this will be executed first.
 }
 
