0e3fee797fbffe7bba153e4b887abf76610b33e4
Asparuh Bambov <akbamboff@gmail.com>
2023-11-27 12:51:19 +0200

    Making some questionable progress.

--- ClassesProject/Assets/Scripts/classesTry.js
+++ ClassesProject/Assets/Scripts/classesTry.js
@@ -1,5 +1,31 @@
 const authorListArr = [];
-const courseId = []
+
+pictures = [
+  'https://i.pravatar.cc/150?img=1',
+  'https://i.pravatar.cc/150?img=2',
+  'https://i.pravatar.cc/150?img=3',
+  'https://i.pravatar.cc/150?img=4',
+  'https://i.pravatar.cc/150?img=5',
+  'https://i.pravatar.cc/150?img=6',
+  'https://i.pravatar.cc/150?img=7',
+  'https://i.pravatar.cc/150?img=8',
+  'https://i.pravatar.cc/150?img=9',
+  'https://i.pravatar.cc/150?img=10',
+  'https://i.pravatar.cc/150?img=11',
+  'https://i.pravatar.cc/150?img=12',
+  'https://i.pravatar.cc/150?img=13',
+  'https://i.pravatar.cc/150?img=14',
+  'https://i.pravatar.cc/150?img=15',
+  'https://i.pravatar.cc/150?img=16',
+  'https://i.pravatar.cc/150?img=17',
+  'https://i.pravatar.cc/150?img=18',
+  'https://i.pravatar.cc/150?img=19',
+];
+
+function getRandomPicture(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 
 class AuthorNameAndCourseCount {
   constructor(id, author,) {
@@ -20,23 +46,44 @@ class AuthorNameAndCourseCount {
     }
   }
 
-  addingCourseCount() {
-    this.addingAndFilteringAuthors();
+
+  render() {
+    this.addingAndFilteringAuthors()
+    const authorSection = document.getElementById('authors');
+    const authorListEl = document.createElement('div');
+    authorListEl.className = 'author-list-element';
     for (const author of authorListArr) {
-      console.log(author.authorName);
-      console.log(author.id)
-      let courseCount = 0;
-      for (const i in data) {
-        if (author.authorName === data[i].author) {
-          courseCount += 1;
-          console.log(author.authorName === data[i].author);
-          courseId.push(data[i].id);
-        };
-      }
+      const authorEl = document.createElement('div');
+      authorEl.className = 'authorLEl';
+      authorEl.innerHTML = `
+      <div id="${
+        author.id
+      }" name="authorElements" class="container mt-2 mb-2 shadow-sm">
+         <div class="lc-block">
+       <img
+         alt="image"
+         class="rounded-circle float-start me-4"
+         src="${pictures[parseInt(getRandomPicture(0, 19))]}"
+         style="width: 5vh"
+         loading="lazy"
+       />
+       <div editable="rich">
+         <h4><strong>${author.authorName}</strong></h4>
+       </div>
+       <h5>Courses: <span class="badge bg-secondary">${
+        "-> COurse Count<-"
+       }</span></h5>
+       <div editable="rich">
+         <p></p>
+       </div>
+     </div>
+   </div>`;
+      
+      authorListEl.append(authorEl)
     }
+    authorSection.append(authorListEl)
   }
 }
 
-new AuthorNameAndCourseCount().addingCourseCount();
+new AuthorNameAndCourseCount().render();
 console.log(authorListArr);
-console.log(courseId);
