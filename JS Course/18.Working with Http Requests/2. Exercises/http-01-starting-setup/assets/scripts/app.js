b72a6f0027d55db0561d7f228415af8a7e6389f8
Asparuh Bambov <akbamboff@gmail.com>
2024-01-10 11:14:20 +0200

    adding some comments to the code.

--- JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
@@ -1,3 +1,4 @@
+// getting elements, so we can work with them.
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
@@ -6,12 +7,15 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+// configuring the responseType, so we can parse the JSON data.
 xhr.responseType = 'json';
 
 xhr.onload = function () {
   // const listOfPosts = JSON.parse(xhr.response);
   const listOfPosts = xhr.response;
   console.log(listOfPosts);
+
+  //sorting the posts and appending them
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
