f74072a60ac12d20915eca19414a149c9397ce8f
Asparuh Bambov <akbamboff@gmail.com>
2024-01-10 11:03:56 +0200

    habba habba

--- JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
@@ -3,9 +3,13 @@
 
  xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
- xhr.onload = function() {
-  const listOfPosts = xhr.response;
+ xhr.responseType = 'json'
 
+ xhr.onload = function() {
+  // const listOfPosts = JSON.parse(xhr.response);
+  const listOfPosts = xhr.response
+  console.log(listOfPosts);
 };
 
  xhr.send();
+ 
