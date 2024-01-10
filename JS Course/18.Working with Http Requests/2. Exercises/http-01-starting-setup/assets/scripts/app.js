a8a5aa504b70f56bc3f15ba7348657c45088a8aa
Asparuh Bambov <akbamboff@gmail.com>
2024-01-10 11:10:19 +0200

    fetching posts from a json file

--- JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
@@ -1,18 +1,23 @@
- // making a GET request
- const xhr = new XMLHttpRequest();
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
 
- xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+// making a GET request
+const xhr = new XMLHttpRequest();
 
- xhr.responseType = 'json'
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
- xhr.onload = function() {
+xhr.responseType = 'json';
+
+xhr.onload = function () {
   // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response
+  const listOfPosts = xhr.response;
   console.log(listOfPosts);
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
 };
 
- xhr.send();
-
- const listElemenenet = document.querySelector('.posts');
- const postTemplate = document.getElementById("single-post")
- 
+xhr.send();
