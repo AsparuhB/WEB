9aee86916f948b4eebe69da545b5b4a4076f38ac
Asparuh Bambov <akbamboff@gmail.com>
2024-01-11 12:29:09 +0200

    adding a delete functionality

--- JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
@@ -3,6 +3,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -33,7 +34,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
-    postEl.querySelector('li').id = post.id
+    postEl.querySelector('li').id = post.id;
     listElement.append(postEl);
   }
 }
@@ -49,16 +50,21 @@ async function createPost(title, content) {
   sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
-fetchButton.addEventListener("click", fetchPosts)
-form.addEventListener('submit', event => {
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', (event) => {
   console.log(event);
   event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
   console.log(enteredTitle);
-  const enteredContent = event.currentTarget.querySelector("#content").value;
+  const enteredContent = event.currentTarget.querySelector('#content').value;
   console.log(enteredContent);
-  
-  createPost(enteredTitle, enteredContent)
+
+  createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener('click', event => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id
+    sendHttpRequest("DELETE", `https://jsonplaceholder.typicode.com/posts/${postId}`)
+  }
 })
-
-
