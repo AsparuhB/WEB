e66a02c8f41eeb6d9e91325649f34aca33f9f2a2
Asparuh Bambov <akbamboff@gmail.com>
2024-01-11 12:18:15 +0200

    adding POST functionality.

--- JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-01-starting-setup/assets/scripts/app.js
@@ -1,6 +1,8 @@
 // getting elements, so we can work with them.
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -46,5 +48,16 @@ async function createPost(title, content) {
   sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
-fetchPosts();
-createPost('DUMMY', 'A dummy post!');
+fetchButton.addEventListener("click", fetchPosts)
+form.addEventListener('submit', event => {
+  console.log(event);
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  console.log(enteredTitle);
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+  console.log(enteredContent);
+  
+  createPost(enteredTitle, enteredContent)
+})
+
+
