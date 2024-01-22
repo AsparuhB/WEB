93f11cfe124faaf8038bf54f1ac6c54fc5dd3bb2
Asparuh Bambov <akbamboff@gmail.com>
2024-01-22 11:36:30 +0200

    making the file.

--- JS Course/18.Working with Http Requests/2. Exercises/http-04-connecting-requests-to-ui/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-04-connecting-requests-to-ui/assets/scripts/app.js
@@ -1,7 +1,5 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
-const form = document.querySelector('#new-post form');
-const fetchButton = document.querySelector('#available-posts button');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -11,7 +9,7 @@ function sendHttpRequest(method, url, data) {
 
     xhr.responseType = 'json';
 
-    xhr.onload = function() {
+    xhr.onload = function () {
       resolve(xhr.response);
       // const listOfPosts = JSON.parse(xhr.response);
     };
@@ -41,17 +39,11 @@ async function createPost(title, content) {
   const post = {
     title: title,
     body: content,
-    userId: userId
+    userId: userId,
   };
 
   sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
-fetchButton.addEventListener('click', fetchPosts);
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector('#title').value;
-  const enteredContent = event.currentTarget.querySelector('#content').value;
-
-  createPost(enteredTitle, enteredContent);
-});
+fetchPosts();
+createPost('Dummy', 'A dummy post!');
