00d934055927ae345ef8fbc28a88c5ae5a00e50f
Asparuh Bambov <akbamboff@gmail.com>
2024-02-01 10:02:25 +0200

    сд

--- JS Course/18.Working with Http Requests/2. Exercises/http-08-post-request-with-fetch/assets/scripts/app.js
+++ JS Course/18.Working with Http Requests/2. Exercises/http-08-post-request-with-fetch/assets/scripts/app.js
@@ -41,41 +41,29 @@ function sendHttpRequest(method, url, data) {
     headers: {
       'Content-Type': 'application/json',
     },
-  })
-    .then((response) => {
-      if (response.status >= 200 && response.status < 300) {
-        return response.json();
-      } else {
-        return response.json().then((errorData) => {
-          console.log(errorData);
-          throw new Error('Something went wrong - server side');
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      throw new Error('Something went wrong!');
-    });
+  }).then(response => {
+    return response.json();
+  });
 }
 
 async function fetchPosts() {
-  try {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  );
-  const listOfPosts = responseData;
-  listElement.innerHTML = '';
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    postEl.querySelector('li').id = post.id;
-    listElement.append(postEl);
-  }
-  } catch (error) {
-    alert(error.message);
-  }
+  // try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    const listOfPosts = responseData;
+    listElement.innerHTML = '';
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+//   } catch (error) {
+//     alert(error.message);
+//   }
 }
 
 async function createPost(title, content) {
